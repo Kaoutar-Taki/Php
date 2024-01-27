3c908b8f6f81a2d573fb02cbf15e6f421f46b89b
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-01-27 11:13:15 +0100

    insert user baseDoner

--- ecommercephp/index.php
+++ ecommercephp/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -8,25 +7,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ktaki</title>
 </head>
-
 <body>
     <?php include 'include/nav.php' ?>
-    <form action="" method="post">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container py-2">
+        <?php
+            if (isset($_POST['add'])){
+                $login = $_POST['login'];
+                $password = $_POST['password'];
+                if (!empty($login) && !empty($password)){
+                    require_once 'include/database.php';
+                    $date_creation = date('Y-m-d');
+                    $sqlState = $pdo->prepare('INSERT INTO utilisateur VALUE(null,?,?,?)');
+                    $sqlState->execute([$login, $password, $date_creation]);
+                }
+                else{
+                    ?>
+                        <div class="alert alert-danger" role="alert">login and password is required</div>
+                    <?php
+                }
+            }
+        ?>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="login" class="form-label">Login</label>
+                <input type="text" class="form-control" id="login" name="login">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary" name="add">Add User</button>
+        </form>
+    </div>
 </body>
-
 </html>
