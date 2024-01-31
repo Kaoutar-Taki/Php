db911505e51b21d3806d854e2fa1a70fa0dcf740
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-01-31 20:34:34 +0100

    add champs image and description and importer image

--- ecommercephp/addProduct.php
+++ ecommercephp/addProduct.php
@@ -19,10 +19,19 @@
             $prix = $_POST['prix'];
             $discount = $_POST['discount'];
             $categorie = $_POST['categorie'];
+            $description = $_POST['description'];  
+
+            $image = "";    
+            if (isset($_FILES['image'])){
+                $image = $_FILES['image']['name'];
+                $fileName = uniqid().$image;
+                move_uploaded_file($_FILES['image']['tmp_name'],'upload/products/'.$fileName) ;
+            }
+
             $date_creation = date('Y-m-d H:i:s');
             if (!empty($libelle) && !empty($prix) && !empty($categorie)) {
-                $sqlState = $pdo->prepare('INSERT INTO produit VALUE(null,?,?,?,?,?)');
-                $sqlState->execute([$libelle, $prix, $discount, $categorie, $date_creation]);
+                $sqlState = $pdo->prepare('INSERT INTO produit VALUE(null,?,?,?,?,?,?,?)');
+                $sqlState->execute([$libelle, $prix, $discount, $categorie, $description, $fileName,$date_creation]);
                 header('location:Products.php');
             } else {
         ?>
@@ -32,10 +41,14 @@
         }
         ?>
         <h4>Add Product</h4>
-        <form action="" method="post">
+        <form action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="libelle" class="form-label">Libelle</label>
                 <input type="text" class="form-control" id="libelle" name="libelle">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" cols="30" rows="10"></textarea>
             </div>
             <div class="mb-3">
                 <label for="prix" class="form-label">Prix</label>
@@ -44,6 +57,10 @@
             <div class="mb-3">
                 <label for="discount" class="form-label">Discount</label>
                 <input type="range" value="0" name="discount" class="form-control" id="discount" min="0" max="90" />
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="file" name="image" class="form-control" id="image" />
             </div>
             <div class="mb-3">
                 <label for="categorie" class="form-label">Categorie</label>
