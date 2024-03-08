e5eab641939e9219b955dbd64ba72bcec01a9b37
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-03-08 23:45:23 +0100

    add backup Assets

---
+++ ecommercephpbackup/assets/js/products/counter.js
@@ -0,0 +1,16 @@
+$('.counter-plus').click(function(e){
+    let Quantity = $(e.currentTarget).siblings('#Quantity');
+    let QuantityValue = parseInt(Quantity.val())+1;
+    if(QuantityValue >=10){
+        QuantityValue = 10;
+    }
+    Quantity.val(QuantityValue);
+})
+$('.counter-minus').click(function(e){
+    let Quantity = $(e.currentTarget).siblings('#Quantity');
+    let QuantityValue = parseInt(Quantity.val())-1;
+    if(QuantityValue < 0){
+        QuantityValue = 0;
+    }
+    Quantity.val(QuantityValue);
+})
