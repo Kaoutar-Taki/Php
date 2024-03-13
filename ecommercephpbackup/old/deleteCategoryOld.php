84fbf6eb99f68bf2bf3a758dbd3c9fb14eab4fb1
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-03-13 15:15:51 +0000

    Delete folder old

--- ecommercephpbackup/old/deleteCategoryOld.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once 'include/database.php';
-    $id = $_GET['id'];
-    $sqlState = $pdo->prepare("DELETE FROM categorie WHERE id = ?");
-    $sqlState->execute([$id]);
-    header('Location: Categories.php');
--- ecommercephpbackup/old/deleteProductOld.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once 'include/database.php';
-    $id = $_GET['id'];
-    $sqlState = $pdo->prepare("DELETE FROM produit WHERE id = ?");
-    $sqlState->execute([$id]);
-    header('Location: Products.php');
