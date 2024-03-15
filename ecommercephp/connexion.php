f776fa71b1454c05f484b8852eafe4479908e6ae
Kaoutar-Taki <takikaoutar01@gmail.com>
2024-03-15 15:14:08 +0000

    la connexion

--- ecommercephp/connexion.php
+++ ecommercephp/connexion.php
@@ -13,16 +13,13 @@
         if (isset($_POST['connexion'])) {
             $login = $_POST['login'];
             $pwd   = $_POST['password'];
-
             if (!empty($login) && !empty($pwd)) {
                 require_once 'include/database.php';
-                $sqlState = $pdo->prepare('SELECT * FROM utilisateur 
-                                                WHERE login=?
-                                                AND   password=?');
+                $sqlState = $pdo->prepare('SELECT * FROM utilisateur  WHERE login=? AND password=?');
                 $sqlState->execute([$login, $pwd]);
                 if ($sqlState->rowCount() >= 1) {
                     $_SESSION['utilisateur'] = $sqlState->fetch();
-                    header('location: admin.php');
+                    header('location: index.php');
                 } else {
         ?>
                     <div class="alert alert-danger" role="alert">
@@ -43,10 +40,8 @@
         <form method="post">
             <label class="form-label">Login</label>
             <input type="text" class="form-control" name="login">
-
             <label class="form-label">Password</label>
             <input type="password" class="form-control" name="password">
-
             <input type="submit" value="Connexion" class="btn btn-primary my-2" name="connexion">
         </form>
     </div>
